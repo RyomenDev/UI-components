310ddaa4b427eb2251e91ca334e231888171f209
RyomenDev <aakashvatsh007@gmail.com>
2025-01-16 17:20:21 +0530

    bigfix:login1A design fix

--- src/components/Login/Login1/Login1A.jsx
+++ src/components/Login/Login1/Login1A.jsx
@@ -10,45 +10,47 @@ const Login = () => {
           {Array.from({ length: 200 }).map((_, index) => (
             <span
               key={index}
-              className="relative block w-[calc(6.14vw_-_2px)] h-[calc(6.25vw_-_2px)] overflow-hidden bg-[#181818] transition-colors duration-[1.5s] hover:bg-[#0f0]"
+              className="relative block w-[calc(6.34vw_-_2px)] h-[calc(6.25vw_-_2px)] overflow-hidden bg-[#181818] transition-colors duration-[1.5s] hover:bg-[#0f0]"
             ></span>
           ))}
         </div>
 
         {/* Sign-in form */}
-        <div className=" absolute w-[400px] p-10 rounded-lg shadow-lg bg-black z-10">
-          <div className="flex flex-col items-center gap-10">
-            <h2 className="text-2xl font-bold uppercase text-green-500">
+        <div className=" absolute w-1/4 h-1/2 px-32 py-16 rounded-3xl shadow-lg bg-black z-10">
+          <div className="flex flex-col items-center justify-center gap-10">
+            <h2 className="text-7xl font-bold uppercase text-green-500">
               Sign In
             </h2>
 
-            <form className="w-full flex flex-col gap-6">
+            <form className="w-full flex flex-col gap-6 text-3xl">
               {/* Username input */}
-              <div className="relative w-full">
+              <div className="relative my-6 w-full">
                 <input
+                  placeholder="Username"
                   type="text"
                   required
-                  className="w-full px-3 py-6 bg-gray-800 text-white rounded-md outline-none focus:ring-2 focus:ring-green-500"
+                  className="w-full px-3 py-7 bg-gray-800 text-white  rounded-md outline-none focus:ring-2 focus:ring-green-500"
                 />
-                <label className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400 pointer-events-none transition-all duration-300 focus-within:translate-y-[-10px] focus-within:text-sm focus-within:text-white">
+                {/* <label className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400 pointer-events-none transition-all duration-300 focus-within:translate-y-[-10px] focus-within:text-sm focus-within:text-white">
                   Username
-                </label>
+                </label> */}
               </div>
 
               {/* Password input */}
               <div className="relative w-full">
                 <input
+                  placeholder=" Password"
                   type="password"
                   required
-                  className="w-full px-3 py-6 bg-gray-800 text-white rounded-md outline-none focus:ring-2 focus:ring-green-500"
+                  className="w-full px-3 py-7 bg-gray-800 text-white rounded-md outline-none focus:ring-2 focus:ring-green-500"
                 />
-                <label className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400 pointer-events-none transition-all duration-300 focus-within:translate-y-[-10px] focus-within:text-sm focus-within:text-white">
+                {/* <label className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400 pointer-events-none transition-all duration-300 focus-within:translate-y-[-10px] focus-within:text-sm focus-within:text-white">
                   Password
-                </label>
+                </label> */}
               </div>
 
               {/* Links */}
-              <div className="flex justify-between text-sm">
+              <div className="flex justify-between text-sm my-5">
                 <a href="#" className="text-gray-400 hover:text-white">
                   Forgot Password
                 </a>
@@ -64,7 +66,7 @@ const Login = () => {
               <div>
                 <button
                   type="submit"
-                  className="w-full py-3 bg-green-500 text-black font-semibold text-lg rounded-md hover:bg-green-600"
+                  className="w-full py-5 bg-green-500 text-black font-semibold  rounded-md hover:bg-green-600"
                 >
                   Login
                 </button>
