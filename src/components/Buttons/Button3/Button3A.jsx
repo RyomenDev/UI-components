4841af72685ef49585c31615de20de8ad984b082
RyomenDev <aakashvatsh007@gmail.com>
2025-01-26 19:14:55 +0530

    Button3A

--- src/components/Buttons/Button3/Button3A.jsx
+++ src/components/Buttons/Button3/Button3A.jsx
@@ -1,13 +1,16 @@
 const Button = () => {
   return (
-    <div className="bg-[#151F28] flex justify-center items-center h-screen">
-      <button className="w-40 h-24 border-4 border-[#149CEA]">
-      {/* <button className="relative w-40 h-14 border-4 border-[#149CEA] bg-transparent text-white font-bold text-lg rounded-md transition-all duration-1000 group overflow-hidden"> */}
-        {/* <span className="absolute top-[-10px] left-[3%] w-[95%] h-[40%] bg-[#151F28] origin-center transition-transform duration-500 group-hover:scale-0" /> */}
-        {/* <span className="absolute top-[80%] left-[3%] w-[95%] h-[40%] bg-[#151F28] origin-center transition-transform duration-500 group-hover:scale-0" /> */}
-        Hover me
-      </button>
-    </div>
+    <>
+      <div className="bg-[#151F28] flex justify-center items-center h-screen">
+        <button
+          className="relative w-40 h-14 border-[3px] border-[#149CEA] bg-transparent text-white font-bold text-lg rounded-md transition-all duration-1000 group overflow-hidden 
+          hover:shadow-[inset_0_0_25px_rgba(20,147,234,1)]"
+          //   style={{ boxShadow: "inset 0 0 25px rgba(255, 0, 0, 0.5)" }}
+        >
+          Hover me
+        </button>
+      </div>
+    </>
   );
 };
 
