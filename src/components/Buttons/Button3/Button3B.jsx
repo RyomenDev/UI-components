50cb302525866ba0802e2de5cf190235f5d058ed
RyomenDev <aakashvatsh007@gmail.com>
2025-01-26 18:22:29 +0530

    Button3B

--- src/components/Buttons/Button3/Button3B.jsx
+++ src/components/Buttons/Button3/Button3B.jsx
@@ -2,7 +2,7 @@ import "./styles.css";
 
 const Button = () => {
   return (
-    <div>
+    <div className="bg-[#151F28] flex justify-center items-center h-screen">
       <button>Hover me</button>
     </div>
   );
