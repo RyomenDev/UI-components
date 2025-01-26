d2b98c866e3d7cfdf9ff87ad48f304365cdfa964
RyomenDev <aakashvatsh007@gmail.com>
2025-01-26 18:46:36 +0530

    //styles.css

--- src/components/Buttons/Button3/Button3A.jsx
+++ src/components/Buttons/Button3/Button3A.jsx
@@ -1,9 +1,10 @@
 const Button = () => {
   return (
     <div className="bg-[#151F28] flex justify-center items-center h-screen">
-      <button className="relative w-40 h-14 border-[3px] border-[#149CEA] bg-transparent text-white font-bold text-lg rounded-md transition-all duration-1000 group overflow-hidden">
-        <span className="absolute top-[-10px] left-[3%] w-[95%] h-[40%] bg-[#151F28] origin-center transition-transform duration-500 group-hover:scale-0" />
-        <span className="absolute top-[80%] left-[3%] w-[95%] h-[40%] bg-[#151F28] origin-center transition-transform duration-500 group-hover:scale-0" />
+      <button className="w-40 h-24 border-4 border-[#149CEA]">
+      {/* <button className="relative w-40 h-14 border-4 border-[#149CEA] bg-transparent text-white font-bold text-lg rounded-md transition-all duration-1000 group overflow-hidden"> */}
+        {/* <span className="absolute top-[-10px] left-[3%] w-[95%] h-[40%] bg-[#151F28] origin-center transition-transform duration-500 group-hover:scale-0" /> */}
+        {/* <span className="absolute top-[80%] left-[3%] w-[95%] h-[40%] bg-[#151F28] origin-center transition-transform duration-500 group-hover:scale-0" /> */}
         Hover me
       </button>
     </div>
--- src/components/Login/Login2/Login2B.jsx
+++ src/components/Login/Login2/Login2B.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./style.css";
+// import "./style.css";
 import {
   RiUser3Line,
   RiLock2Line,
