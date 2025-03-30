58a972c82a034e636fd77397a6783852334f9a00
RyomenDev <aakashvatsh007@gmail.com>
2025-03-30 17:08:50 +0530

    projects-accord

--- src/Advanced/containers/projectsAccord/Projects.jsx
+++ src/Advanced/containers/projectsAccord/Projects.jsx
@@ -1,11 +1,6 @@
-// import React from "react";
-// import Header from "../../components/header/Header";
-// import Footer from "../../components/footer/Footer";
-// import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
 // import PublicationCard from "../../components/publicationsCard/PublicationCard";
 // import Button from "../../components/button/Button";
 // import TopButton from "../../components/topButton/TopButton";
-// import ProjectsData from "../../shared/opensource/projects.json";
 import {
   //   greeting,
   projectsHeader,
@@ -44,67 +39,6 @@ const Projects = ({ theme, onToggle }) => {
         </motion.div>
       </div>
 
-      {/* <div className="repo-cards-div-main">
-        {ProjectsData.data.map((repo, index) => (
-          <motion.div
-            key={repo.id || index}
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-          >
-            <GithubRepoCard repo={repo} theme={theme} />
-          </motion.div>
-        ))}
-      </div> */}
-
-      {/* <Button
-        text={"More Projects"}
-        className="project-button"
-        href={greeting.githubProfile}
-        newTab={true}
-        theme={theme}
-      /> */}
-
-      {/* {publications.data.length > 0 && (
-        <motion.div
-          className="basic-projects"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-        >
-          <div className="publications-heading-div">
-            <div className="publications-heading-text-div">
-              <h1
-                className="publications-heading-text"
-                style={{ color: theme.text }}
-              >
-                {publicationsHeader.title}
-              </h1>
-              <p
-                className="projects-header-detail-text subTitle"
-                style={{ color: theme.secondaryText }}
-              >
-                {publicationsHeader.description}
-              </p>
-            </div>
-          </div>
-        </motion.div>
-      )} */}
-
-      {/* <div className="repo-cards-div-main">
-        {publications.data.map((pub, index) => (
-          <motion.div
-            key={pub.id || index}
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-          >
-            <PublicationCard pub={pub} theme={theme} />
-          </motion.div>
-        ))}
-      </div> */}
-
-      {/* <Footer theme={theme} onToggle={onToggle} /> */}
       {/* <TopButton theme={theme} /> */}
     </div>
   );
