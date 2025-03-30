9dfd2dcb94245e894abb1928ff7b6494c38a03c4
RyomenDev <aakashvatsh007@gmail.com>
2025-03-30 11:19:47 +0530

    startup:loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { useEffect, useState } from "react";
+import SplashScreen from "./Advanced/containers/splashScreen/SplashScreen";
+import { StyleProvider } from "./Advanced/contexts/StyleContext.js";
+import { useLocalStorage } from "./Advanced/hooks/useLocalStorage";
+import { splashScreen } from "./Advanced/portfolio";
+// import Education from "./Advanced/containers/education/Education.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+  const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
+    useState(true);
+  useEffect(() => {
+    if (splashScreen.enabled) {
+      const splashTimer = setTimeout(
+        () => setIsShowingSplashAnimation(false),
+        splashScreen.duration
+      );
+      return () => {
+        clearTimeout(splashTimer);
+      };
+    }
+  }, []);
+
+  const changeTheme = () => {
+    setIsDark(!isDark);
+  };
+  return (
+    <>
+      <div className={isDark ? "dark-mode" : null}>
+        <StyleProvider value={{ isDark: isDark, changeTheme: changeTheme }}>
+          {isShowingSplashAnimation && splashScreen.enabled ? (
+            <SplashScreen />
+          ) : (
+            <>
+              Hii
+              {/* <Education /> */}
+            </>
+          )}
+        </StyleProvider>
+      </div>
+    </>
+  );
+}
+
+export default App;
